a0837a87628ba584401f668b5719264b7cfbf7ae
priyanshumay <tiwarimay2002@gmail.com>
2021-01-06 15:11:43 +0530

    updating

--- setup.sh
+++ setup.sh
@@ -89,175 +89,281 @@ On_IWhite='\033[0;107m'   # White
 
 
 progressbar() {
-  local duration
-  local columns
-  local space_available
-  local fit_to_screen  
-  local space_reserved
-
-  space_reserved=6
-  duration=20
-  columns=$(tput cols)
-  space_available=$(( columns-space_reserved ))
-
-  if (( duration < space_available )); then 
-  	fit_to_screen=1; 
-  else 
-    fit_to_screen=$(( duration / space_available )); 
-    fit_to_screen=$((fit_to_screen+1)); 
-  fi
-
-  already_done() { for ((done=0; done<(elapsed / fit_to_screen) ; done=done+1 )); do printf "▇"; done }
-  remaining() { for (( remain=(elapsed/fit_to_screen) ; remain<(duration/fit_to_screen) ; remain=remain+1 )); do printf " "; done }
-  percentage() { printf "| %s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
-  clean_line() { printf "\r"; }
-
-  for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
-      already_done; remaining; percentage
-      sleep 0.1
-      clean_line
-  done
-  clean_line
+    local duration
+    local columns
+    local space_available
+    local fit_to_screen
+    local space_reserved
+    
+    space_reserved=6
+    duration=20
+    columns=$(tput cols)
+    space_available=$(( columns-space_reserved ))
+    
+    if (( duration < space_available )); then
+        fit_to_screen=1;
+    else
+        fit_to_screen=$(( duration / space_available ));
+        fit_to_screen=$((fit_to_screen+1));
+    fi
+    
+    already_done() { for ((done=0; done<(elapsed / fit_to_screen) ; done=done+1 )); do printf "▇"; done }
+    remaining() { for (( remain=(elapsed/fit_to_screen) ; remain<(duration/fit_to_screen) ; remain=remain+1 )); do printf " "; done }
+    percentage() { printf "| %s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+    
+    for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
+        already_done; remaining; percentage
+        sleep 0.1
+        clean_line
+    done
+    clean_line
 }
 
 countdown() {
-  secs=$1
-  shift
-  msg=$@
-  while [ $secs -gt 0 ]
-  do
-    printf "\r\033[K$msg in %.d seconds" $((secs--))
-    sleep 1
-  done
-  echo
+    secs=$1
+    shift
+    msg=$@
+    while [ $secs -gt 0 ]
+    do
+        printf "\r\033[K${IRed}$msg in %.d seconds" $((secs--))
+        sleep 1
+    done
 }
 
-pause(){
-	local m="$@"
-	echo "$m"
-	read -p "Press [Enter] key to continue..." key
-}
 
 ubuntu(){
-
-cat /etc/lsb-release
-countdown 5 Installation starting
-while :
-do
-
-	clear
-	echo "---------------------------------"
-	echo "	  Installation Menu            "
-	echo "---------------------------------"
-	echo "1. Clang"
-	echo "2. VS Code"
-	echo "3. Sublime Text"
-	echo "4. Libre Office"
-	echo "5. LAMP Stack"
-	echo "6. MEAN Stack"
-	echo "7. NodeJS"
-	echo "8. Golang"
-	echo "9. Virtual Box"
-	echo "10. qBittorrent"
-	echo "11. Kazam"
-	echo "12. Chrome"
-	echo "13. Chromium"
-	echo "14. Mozilla Firefox"
-	echo "15. Brave"
-	echo "16. VLC"
-	echo "17. Vim"
-	echo "q. Exit"
-	echo "---------------------------------"
-	read -r -p "Enter your choice [1-5] : " c
-	# take action
-	case $c in
-		1) pause "$(date)";;
-	esac
-done
-
-	while true
-do
-	echo -e "${BPurple} Enter your choice [vlc,chrome,chromium,lamp,firefox,brave,vim,pip] ?${Reset}"
-	read input
-
-
-if [ $input == "lamp" ]; then
-	echo "Installing Apache2"
-	sudo apt update
-	sudo apt install apache2 -y
-	sudo apache2ctl configtest
-
-	echo "Adjusting Firewall"
-	sudo ufw app list
-	sudo ufw app info "Apache Full"
-	sudo ufw allow in "Apache Full"
-
-	echo "Installing Mysql"
-	sudo apt install mysql-server -y
-
-	echo "Installing PHP"
-	sudo apt install php libapache2-mod-php php-mcrypt php-mysql -y
-
-	echo "Inastalling phpmyadmin..."
-	sudo apt-get update
-	sudo apt-get install phpmyadmin php-mbstring php-gettext
-	sudo phpenmod mcrypt
-	sudo phpenmod mbstring
-	sudo systemctl restart apache2
-
-	sudo systemctl start apache2
-	echo "Installation completed"
-	sudo systemctl status apache2
-
-elif [ $input == "chrome" ]; then
-	echo "Installing latest version of Chrome"
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
-	echo "Installation completed"
-
-elif [ $input == "chromium" ] ; then
-	echo "Installing latest version of Chromium"
-	sudo apt install -y chromium-browser
-	echo "Installation completed"
-
-elif [ $input == "firefox" ]; then
-	echo "Installing latest version of firefox"
-	sudo apt install -y firefox
-	echo "Installation completed"
-
-elif [ $input == "brave" ]; then
-	echo "Installing latest version of Brave"
-	echo "But firstly installling the prequisites"
-	sudo apt install apt-transport-https curl
-	curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-	echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-
-elif [ $input == "vlc" ]; then
-	echo "Installing latest version of VLC"
-	sudo apt-get install vim
-	echo "Installation completed"
-
-elif [ $input == "vim" ]; then
-	echo "Installing latest version of VIM"
-	sudo apt-get install vim
-	echo "Installation completed"
-
-elif [ $input == "pip" ]; then
-
-	echo "Installing pip3"
-	sudo apt install python3-pip
-	echo "Installation successfully completed"
-
-else 
-
-	echo -e "${BGreen}Quiting ..."
-	echo "Tschüss"
-	break;
-
-fi
-
-done
+    
+    echo -e "${IGreen}---------------------------------"
+    echo "	  Distro Information           "
+    echo "---------------------------------"
+    echo ""
+    cat /etc/lsb-release
+    echo -e "${Reset}"
+    
+    countdown 3 Installation starting
+    
+    while :
+    do
+        
+        clear
+        echo -e "${IWhite}---------------------------------"
+        echo "	  Installation Menu            "
+        echo "---------------------------------"
+        echo "1. Clang"
+        echo "2. VS Code"
+        echo "3. Sublime Text"
+        echo "4. Libre Office"
+        echo "5. LAMP Stack"
+        echo "6. MEAN Stack"
+        echo "7. NodeJS"
+        echo "8. Golang"
+        echo "9. Virtual Box"
+        echo "10. qBittorrent"
+        echo "11. Kazam"
+        echo "12. Chrome"
+        echo "13. Chromium"
+        echo "14. Mozilla Firefox"
+        echo "15. Brave"
+        echo "16. VLC"
+        echo "17. Vim"
+        echo "18. Emacs"
+        echo -e "Enter ${URed}quit${Reset} or Ctrl+C to exit"
+        echo "---------------------------------"
+        read -r -p "Enter your choice [1-18] : " c
+        # take action
+        case $c in
+            1)
+                echo -e "${IGreen}Installing latest version of Clang"
+                echo -e "${Reset}"
+                sudo apt install clang
+                echo -e "${IGreen}Installation completed"
+                
+            ;;
+            
+            2)
+                echo -e "${IGreen}Installing latest version of VS Code"
+                echo -e "${Reset}"
+                sudo apt install software-properties-common apt-transport-https wget
+                wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+                sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+                sudo apt update
+                sudo apt install code
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            3)
+                echo -e "${IGreen}Installing latest version of Sublime Text"
+                echo -e "${Reset}"
+                
+                sudo apt install apt-transport-https ca-certificates curl software-properties-common
+                curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+                sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+                sudo apt update
+                sudo apt install sublime-text
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            4)
+                echo -e "${IGreen}Installing latest version of Libre Office"
+                echo -e "${Reset}"
+                sudo add-apt-repository ppa:libreoffice/ppa
+                sudo apt update
+                sudo apt install libreoffice
+                
+                
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            5)
+                echo -e "${IGreen}Installing latest version of LAMP"
+                echo -e "${Reset}"
+                echo "Installing Apache2"
+                sudo apt update
+                sudo apt install apache2 -y
+                sudo apache2ctl configtest
+                
+                echo "Adjusting Firewall"
+                sudo ufw app list
+                sudo ufw app info "Apache Full"
+                sudo ufw allow in "Apache Full"
+                
+                echo "Installing Mysql"
+                sudo apt install mysql-server -y
+                
+                echo "Installing PHP"
+                sudo apt install php libapache2-mod-php php-mcrypt php-mysql -y
+                
+                echo "Inastalling phpmyadmin..."
+                sudo apt-get update
+                sudo apt-get install phpmyadmin php-mbstring php-gettext
+                sudo phpenmod mcrypt
+                sudo phpenmod mbstring
+                sudo systemctl restart apache2
+                
+                sudo systemctl start apache2
+                echo "Installation completed"
+                sudo systemctl status apache2
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            6)
+                echo -e "${IGreen}Installing MongoDB ]"
+                echo -e "${Reset}"
+                sudo apt-get install gnupg -y
+                wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+                sudo apt update
+                echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+                sudo apt-get install -y mongodb-org
+                sudo systemctl start mongod
+                service mongod status
+                
+                echo -e "${IGreen}Installing NodeJS "
+                echo -e "${Reset}"
+                curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash –
+                sudo apt install -y nodejs
+                sudo apt install build-essential
+                
+            echo "MEAN stack Installed";;
+            
+            7)
+                echo -e "${IGreen}Installing latest version of fffff"
+                echo -e "${Reset}"
+                
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            8)
+                echo -e "${IGreen}Installing latest version of Golang"
+                echo -e "${Reset}"
+                wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+                tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+                export PATH=$PATH:/usr/local/go/bin
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            9)
+                echo -e "${IGreen}Installing latest version of Virtual Box"
+                echo -e "${Reset}"
+                sudo apt-get update
+                sudo apt-get install virtualbox
+                sudo apt-get install virtualbox—ext–pack
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            10)
+                echo -e "${IGreen}Installing latest version of Kazam"
+                echo -e "${Reset}"
+                sudo apt install kazam
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            11)
+                echo -e "${IGreen}Installing latest version of qBittorrent"
+                echo -e "${Reset}"
+                sudo apt install qbittorrent
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            
+            12)
+                echo -e "${IGreen}Installing latest version of Chrome"
+                echo -e "${Reset}"
+                wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                sudo dpkg -i google-chrome-stable_current_amd64.deb
+                echo -e "${IGreen}Installation completed"
+            ;;
+            
+            13)
+                echo -e "${IGreen}Installing latest version of Chromium"
+                echo -e "${Reset}"
+                sudo apt install -y chromium-browser
+            echo -e "${IGreen}Installation completed";;
+            
+            
+            14)
+                echo -e "${IGreen}Installing latest version of firefox"
+                echo -e "${Reset}"
+                sudo apt install -y firefox
+            echo -e "${IGreen}Installation completed";;
+            
+            15)
+                echo -e "${IGreen}Installing latest version of Brave"
+                echo "But firstly installling the prequisites"
+                echo -e "${Reset}"
+                sudo apt install apt-transport-https curl
+                curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+                echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+            ;;
+            
+            16)
+                echo -e "${IGreen}Installing latest version of VLC"
+                echo -e "${Reset}"
+                sudo apt-get install vim
+            echo -e "${IGreen}Installation completed";;
+            
+            17)
+                echo -e "${IGreen}Installing latest version of VIM"
+                echo -e "${Reset}"
+                sudo apt-get install vim
+            echo -e "${IGreen}Installation completed";;
+            
+            18)
+                echo -e "${IGreen}Installing latest version of EMACS"
+                echo -e "${Reset}"
+                sudo add-apt-repository ppa:kelleyk/emacs
+                sudo apt-get update
+                sudo apt install emacsVERSION
+            echo -e "${IGreen}Installation completed";;
+            
+            quit)
+                echo -e "${BGreen}Quiting ..."
+                echo "Bye"
+            break;;
+            *) Pause "Select between 1 to 18 only"
+        esac
+    done
+    
 }
 
 echo ""
@@ -269,11 +375,10 @@ echo " |                                                       | "
 echo " ========================================================= "
 echo ""
 echo -e "${UGreen}https://ahampriyanshu.github.io"
-echo "ahampriyanshu@gmail.com"
+echo "mailto:ahampriyanshu@gmail.com"
 echo -e "${Reset}"
-
+echo -e "${ICyan}Detecting System Configuration"
+echo -e "${Reset}"
 progressbar
-
-echo ""
-
-test -f /etc/lsb-release && ubuntu || echo "Oh No! looks like you are using some distro other than Ubuntu"
+echo -e ""
+(test -f /etc/lsb-release && ubuntu) || (test -f /etc/debian_version && kali) || echo "Oh No! looks like you are using some distro other than Ubuntu or Kali"
