0a623c8e4c70e2153fc92261505e4f4fb8264b4d
Priyanshu Tiwari <ahampriyanshu@gmail.com>
2021-01-23 18:39:06 +0530

    yt_dl_gui => extending this project

--- youtube_dl_gui/yt_dl_gui.py
+++ youtube_dl_gui/yt_dl_gui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 from pytube import *
 from tkinter import *
@@ -5,6 +6,15 @@ from tkinter.filedialog import *
 from tkinter.messagebox import *
 import tkinter.font as tkfont
 from threading import *
+import re
+regex = re.compile(
+        r'^(?:http|ftp)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' 
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
 
 file_size = 0
 
@@ -13,7 +23,12 @@ def progess(chunk, file_handle, bytes_remaining):
     file_downloaded = (file_size - bytes_remaining)
     per = (file_downloaded/file_size)*100
     per_com.config(text=f'{per:.2f} %')
-    byte_com.config(text=f'{file_downloaded:.2f} %')
+    byte_com.config(text=f'{file_downloaded/(1024*1024):.2f}/{file_size/(1024*1024):.2f} MiB')
+
+def getName(linkk):
+    tube = YouTube(linkk)
+    video = tube.streams.first()
+    return video.title
 
 
 def getVideo():
@@ -49,9 +64,14 @@ def pickfolder():
     chosenfolder.set(filename)
 
 def add():
-    queue.insert(END, linkText.get())
-    downloadqueue.append(linkText.get())
-    linkText.delete(0, END)
+    link = linkText.get()
+    if re.match(regex, link) is not None:
+        listbox.insert(END, link)
+        downloadqueue.append(link)
+        linkText.delete(0, END)
+    else:
+        showinfo("Error","Invalid URL")
+
 
 
 root = Tk()
@@ -77,12 +97,17 @@ destinationText.grid(row=2,   column=1,     pady=5,  padx=5, columnspan=2)
 browse_B = Button(root, text="Browse", command=pickfolder,   width=10, fg="#ffffff",  bg="#273239")
 browse_B.grid(row=2,  column=3,   pady=1, padx=5)
 
+listbox = Listbox(root )
+vsb = Scrollbar(root, orient="vertical", command=listbox.yview)
+vsb.grid(row=0, column=1, sticky='ns')
+listbox.configure(yscrollcommand=vsb.set)
 
-Download_B = Button(root, text="Download", command=startDownload,    width=20, fg="#ffffff",   bg="#273239")
-Download_B.grid(row=3,   column=1,   pady=3,   padx=3)
-queue = Listbox(root)
 downloadqueue = []
-queue.grid(column=0, row=4, columnspan=3,padx=40, sticky=W+E)
+listbox.grid(column=0, row=3, columnspan=6)
+delBtn = Button(root, text = "delete", command = lambda listbox=listbox: listbox.delete(ANCHOR)) 
+delBtn.grid(row=5,column=1,pady=3,padx=3) 
+Download_B = Button(root, text="Download", command=startDownload,    width=20, fg="#ffffff",   bg="#273239")
+Download_B.grid(row=5,column=2,pady=3,padx=3)
 # my_font = tkfont.Font(family = "Helvetica", size = 18,weight = "bold")
 # label1 = Label(root,text = "Enter URL",width = 10,fg = "#273239", bg = "#ffffff")
 # label1.config(font = my_font)
@@ -91,10 +116,11 @@ queue.grid(column=0, row=4, columnspan=3,padx=40, sticky=W+E)
 # url.pack(side=TOP,fill=X,padx=30)
 # Download_B = Button(root,text="Download",font=("verdana",18),fg = "white",bg = "#273239",relief='ridge',command=startDownload)
 # Download_B.pack(side=TOP,pady=20)
-title_label = Label(root, text="",font = my_font, bg="#ffffff",pady=10)
-title_label.grid(row=6, column=0, columnspan=1, pady=5,  padx=5)
-per_com = Label(root, text="",font = my_font, bg="#ffffff",pady=10)
+m_font = tkfont.Font(family = "Helvetica", size = 10)
+title_label = Label(root,width=15, text="Title fjdsjfldsjf fmdklfjlkds ndskfdsklfj",font = m_font, bg="#ffffff",pady=5)
+title_label.grid(row=6, column=0, pady=5,  padx=5, sticky=W)
+per_com = Label(root, text="",font = m_font, bg="#ffffff")
 per_com.grid(row=6, column=2, pady=5,  padx=5)
-byte_com = Label(root, text="",font = my_font, bg="#ffffff",pady=10)
+byte_com = Label(root, text="",font = m_font, bg="#ffffff")
 byte_com.grid(row=6, column=3, pady=5,  padx=5)
 root.mainloop()
