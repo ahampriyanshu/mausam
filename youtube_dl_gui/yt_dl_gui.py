c91f4680915d7b5b3cf7438e86fc7f6ed961d55f
Priyanshu Tiwari <ahampriyanshu@gmail.com>
2021-01-24 15:46:25 +0530

    yt_dl_gui => about to finish, just kidding

--- youtube_dl_gui/yt_dl_gui.py
+++ youtube_dl_gui/yt_dl_gui.py
@@ -7,6 +7,7 @@ from tkinter.messagebox import *
 import tkinter.font as tkfont
 from threading import *
 from tkinter import ttk 
+import ffmpeg
 import re
 regex = re.compile(
         r'^(?:http|ftp)s?://'
@@ -18,18 +19,19 @@ regex = re.compile(
 
 
 file_size = 0
-total = 0
-downloadqueue = [[]]
-choices = ("720p","360p","144p","audio")
+downloadqueue = []
+resolution = []
+choices = ("2160p","1440p","1080p", "720p","360p","144p","mp3")
+
+def convert(seconds): 
+    min, sec = divmod(int(seconds), 60) 
+    return "%02d:%02d" % (min, sec) 
 
 def progess(chunk, file_handle, bytes_remaining):
-    global total
     file_downloaded = (file_size - bytes_remaining)
-    total += file_downloaded
     per = (file_downloaded/file_size)*100
-    per_com.config(text=f'{per:.2f} %')
-    byte_com.config(text=f'{file_downloaded/(1024*1024):.2f}/{file_size/(1024*1024):.2f} MiB')
-    total_com.config(text=f'{total/(1024*1024):.2f} MiB')
+    progess.config(text=f'{per:.2f} %')
+    size.config(text=f'{file_downloaded/(1024*1024):.2f}/{file_size/(1024*1024):.2f} mb')
 
 def getName(url):
     tube = YouTube(url)
@@ -40,27 +42,31 @@ def getName(url):
 def getVideo():
     global file_size
     i = 1
+    task = len(downloadqueue)
     while not downloadqueue == [] :
         url = downloadqueue.pop()
+        res = resolution.pop()
         try:
-            Download_B.config(text="Please wait ...")
+            Download_B.config(text=f'Downloading {i} out of {task}')
             Download_B.config(state=DISABLED)
-            path_to_save_video = chosenfolder.get()
+            path_to_save_video = location.get()
             if path_to_save_video is None:
                 showinfo("Error","Invalid Path")
                 return
             tube = YouTube(url, on_progress_callback=progess)
             video = tube.streams.first()
             file_size = video.filesize
-            title_label.config(text=f'{i} out of {len(downloadqueue)}')
+            duration.config(text=convert(tube.length))
+            channel.config(text=tube.author.lower())
             video.download(path_to_save_video)
-            Download_B.config(text="Start Download")
-            Download_B.config(state=NORMAL)
-            title_label.pack_forget()
 
         except Exception as e:
             print(e)
+        listbox.delete(END)
         i+1
+
+    Download_B.config(text="Start Download")
+    Download_B.config(state=NORMAL)
 
 
 def startDownload():
@@ -70,17 +76,20 @@ def startDownload():
 
 def pickfolder():
     filename = askdirectory()
-    chosenfolder.set(filename)
+    location.set(filename)
 
 def checkadd():
-    add_B.config(text="Please wait ...")
+    add_B.config(text="Adding...")
     add_B.config(state=DISABLED)
     link = linkText.get()
     if re.match(regex, link) is not None:
-        listbox.insert(END, getName(link))
-        downloadqueue.append((link,monthchoosen.get()))
-        print(downloadqueue)
-        linkText.delete(0, END)
+        if link in downloadqueue:
+            showinfo("Error","link already exists")
+        else:
+            listbox.insert(END, getName(link))
+            downloadqueue.append(link)
+            resolution.append(monthchoosen.get())
+            linkText.delete(0, END)
     else:
         showinfo("Error","Invalid URL")
     add_B.config(text="Add")
@@ -98,7 +107,7 @@ root.geometry("600x500")
 root.resizable(False, False)
 root.config(background="#ffffff")
 video_Link = StringVar()
-chosenfolder = StringVar()
+location = StringVar()
 
 # create all of the main containers
 top_frame = Frame(root,bg="#ffffff")
@@ -118,12 +127,12 @@ linkText = Entry(top_frame, width=25)
 linkText.grid(row=1, column=1, pady=5, sticky=W)
 monthchoosen = ttk.Combobox(top_frame, values=choices, width=8, background="#273239", foreground="black")
 monthchoosen.place(x=330, y=15)
-monthchoosen.current(0)  
+monthchoosen.current(3)  
 add_B = Button(top_frame, text="Add", command=add,   width=10, fg="#ffffff",  bg="#273239")
 add_B.grid(row=1,  column=3,   pady=1,)
-destination_label = Label(top_frame,  text="Destination",font = my_font, bg="#ffffff",pady=10)
+destination_label = Label(top_frame,  text="Location",font = my_font, bg="#ffffff",pady=10)
 destination_label.grid(row=2,  column=0, pady=5, padx=5)
-destinationText = Entry( top_frame,   width=40,   textvariable=chosenfolder)
+destinationText = Entry( top_frame,   width=40,   textvariable=location)
 destinationText.grid(row=2,   column=1, pady=5,  padx=5)
 browse_B = Button(top_frame, text="Browse", command=pickfolder,   width=10, fg="#ffffff",  bg="#273239")
 browse_B.grid(row=2,  column=3,   pady=1, padx=5)
@@ -142,22 +151,22 @@ Download_B = Button(bottom_frame, text="Download", command=startDownload, width=
 Download_B.grid(row=5,column=2,pady=3,padx=3)
 
 m_font = tkfont.Font(family = "verdana", size = 12, weight="bold")
-Label(bottom2_frame, width=10, font = my_font , text="Progess", bg="#fff").grid(row=0, column=0, padx=10, pady=10)
-Label(bottom2_frame, width=10, font = my_font , text="Type", bg="#fff").grid(row=0, column=1, padx=10, pady=10)
-Label(bottom2_frame, width=10, font = my_font , text="Completed", bg="#fff").grid(row=0, column=2, padx=10, pady=10)
-Label(bottom2_frame, width=10, font = my_font , text="Size", bg="#fff").grid(row=0, column=3, padx=10, pady=10)
-Label(bottom2_frame, width=10, font = my_font , text="Total", bg="#fff").grid(row=0, column=5, padx=10, pady=10)
+Label(bottom2_frame, width=10, font = my_font , text="channel", bg="#fff").grid(row=0, column=0, padx=10, pady=10)
+Label(bottom2_frame, width=10, font = my_font , text="quality", bg="#fff").grid(row=0, column=1, padx=10, pady=10)
+Label(bottom2_frame, width=10, font = my_font , text="duration", bg="#fff").grid(row=0, column=2, padx=10, pady=10)
+Label(bottom2_frame, width=10, font = my_font , text="progress", bg="#fff").grid(row=0, column=3, padx=10, pady=10)
+Label(bottom2_frame, width=10, font = my_font , text="size", bg="#fff").grid(row=0, column=5, padx=10, pady=10)
 
 m_font = tkfont.Font(family = "verdana", size = 10)
-title_label = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
-title_label.grid(row=1, column=0, padx=10, pady=10)
-resol = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
-resol.grid(row=1, column=1, padx=10, pady=10)
-per_com = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
-per_com.grid(row=1, column=2, padx=10, pady=10)
-byte_com = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
-byte_com.grid(row=1, column=3, padx=10, pady=10)
-total_com = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
-total_com.grid(row=1, column=5, padx=10, pady=10)
+channel = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
+channel.grid(row=1, column=0, padx=10, pady=10)
+quality = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
+quality.grid(row=1, column=1, padx=10, pady=10)
+duration = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
+duration.grid(row=1, column=2, padx=10, pady=10)
+progress = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
+progress.grid(row=1, column=3, padx=10, pady=10)
+size = Label(bottom2_frame, width=10, font = my_font , text="", bg="#fff")
+size.grid(row=1, column=5, padx=10, pady=10)
 
 root.mainloop()
